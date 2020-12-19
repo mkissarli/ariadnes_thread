7cd96aaafe351aec9a553b462070f1ebc5a12adf
mkissarli <kissarlim@gmail.com>
2020-12-19 14:05:02 +0000

    Got dash working. BFS is wonky.

---
+++ app.py
@@ -0,0 +1,105 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_cytoscape as cyto
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+from thread.logic import *
+#comp = start_at_company("07798925")
+
+def return_company_graph(graph):
+    elements = []
+    for key, value in graph.graph_dict.items():
+        if type(key) is Company:
+            elements.append({"data":
+                             {
+                                 "id": key.company_name,
+                                 "label": key.company_name
+                             },
+                             "classes": "company"})
+            for elem in value:
+                elements.append({"data":
+                                 {
+                                     "source": key.company_name,
+                                     "target": elem.officer_name
+                                 }})
+        else:
+            elements.append({"data":
+                             {
+                                 "id": key.officer_name,
+                                 "label": key.officer_name
+                             },
+                             "classes": "officer"})
+            for elem in value:
+                elements.append({"data":
+                                 {
+                                     "source": key.officer_name,
+                                     "target": elem.company_name
+                                 }})
+
+    return elements
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    #[
+    #[html.H1("Company Graph")]
+    [
+        html.Label("Company Number: "),
+        dcc.Input(
+            id = "input_cn",
+            placeholder='Enter company number',
+            type='text',
+            value=''
+        ),
+        html.Label("Depth: "),
+        dcc.Input(
+            id = "input_depth",
+            placeholder='Enter company number',
+            type='number',
+            value='1'
+        ),
+        html.Div(id = "output")
+    ]
+)
+
+@app.callback( 
+    Output(component_id ='output', component_property ='children'), 
+    [Input(component_id ='input_cn', component_property ='value'),
+    Input(component_id ='input_depth', component_property ='value')]
+)
+
+def update_value(input_cn, input_depth): 
+    try:
+        comp = start_at_company(input_cn, depth = int(input_depth))
+        return cyto.Cytoscape(
+            id='cytoscape',
+            elements=return_company_graph(comp),
+            layout={'name': 'breadthfirst'},
+            style={'width': '100%', 'height': '480px'},
+            stylesheet=[
+                {
+                    'selector': 'node',
+                    'style': {
+                        'content': 'data(label)'
+                    }
+                },
+                {
+                    'selector': '.officer',
+                    'style': {
+                    'background-color': 'red',
+                    }
+                },
+                {
+                    'selector': '.company',
+                    'style': {
+                        'background-color': 'blue',
+                    }
+                }
+            ])
+    except: 
+        return "Error, the input is not valid"
+
+app.run_server(debug=True)
+
--- thread/logic.py
+++ thread/logic.py
@@ -1,54 +1,85 @@
 import requests
 from requests.auth import HTTPBasicAuth
-
-api_key = "yLwgnyHvwlYxkbOBAoLEwsaEfVQ_a7kAuCUTNtSt"
-auth = HTTPBasicAuth("{key}".format(key = api_key), '')
-
-# Note Company_no has to be str as it has leading 0s
-
-def get_company_info(company_number: str):
-    """
-    Retrieves information about a company from Companies House.
-    Args:
-        company_no (str): Registered company number.
-    Returns:
-        Information Companies House holds on the company.
-    """
-
-    url = "https://api.companieshouse.gov.uk/company/{company_num}".format(company_num = company_number)
-    response = requests.get(url, auth = auth)
-    return response.json()
-
-def get_company_officers(company_number: str):
-    url = "https://api.companieshouse.gov.uk/company/{company_num}/officers".format(company_num = company_number)
-    response = requests.get(url, auth = auth)
-    return response.json()
-    
 from collections import deque
+
+class API:
+    api_key = "yLwgnyHvwlYxkbOBAoLEwsaEfVQ_a7kAuCUTNtSt"
+    base = "https://api.companieshouse.gov.uk"
+    auth = HTTPBasicAuth("{key}".format(key = api_key), '')
+
+    def get_company_info(company_number: str):
+        """
+        Retrieves information about a company from Companies House.
+        Args:
+        company_no (str): Registered company number.
+        Returns:
+        Information Companies House holds on the company.
+        """
+        
+        url = "{base}/company/{company_num}".format(base = API.base, company_num = company_number)
+        response = requests.get(url, auth = API.auth)
+        return response.json()
+    
+    def get_company_officers(company_number: str):
+        """
+        Retrieves information about officers from Companies House.
+        Args:
+        company_no (str): Registered company number.
+        Returns:
+        Information Companies House holds on the company's officers.
+        """
+        
+        url = "{base}/company/{company_num}/officers".format(base = API.base, company_num = company_number)
+        response = requests.get(url, auth = API.auth)
+        return response.json()
+    
+    def get_general(link: str):
+        """
+        Retrieves information from a link on Companies House.
+        Args:
+        company_no (str): Registered company number.
+        Returns:
+        Information from Companies House
+        """
+        url = "{base}{link}".format(base = API.base, link = link)
+        response = requests.get(url, auth = API.auth)
+        return response.json()
 
 def start_at_company(company_number: str, depth: int = 1):
     company_graph = Graph()
     officer_count = 0
-    q = deque([])
-    q.append([company_number])
+    q = deque(set())
+    q.append({company_number})
+    depth = depth + 1
     while len(q) > 0 and depth > 0:
         number_list = q.popleft()
-        new_number_list = []
+        new_number_list = set()
         for number in number_list:
             # If the value already exists in the graph then skip the rest of the loop.
-            if len({k: v for k, v in company_graph.graph_dict.items()
-                    if type(v) == Company and v.company_number == number}):
-                continue
-
-            response = get_company_info(number)
+            #if len({k: v for k, v in company_graph.graph_dict.items()
+            #        if type(v) == Company and v.company_number == number}):
+            #    continue
+            
+            response = API.get_company_info(number)
             c = Company(response)
-            response = get_company_officers(number)
-            for i in response["items"]:
-                o = Officer(i)
-                company_graph.add(c, o)
+            depth = depth - 1
+            
+            response = API.get_company_officers(number)
+            if depth > 0:
+                depth = depth - 1
+                for i in response["items"]:
+                    o = Officer(i)
+                    company_graph.add(c, o)
+                    if depth > 0:
+                        cur_response = API.get_general(i["links"]["officer"]["appointments"])
+                        for appointments in cur_response["items"]:
+                            new_num = appointments["appointed_to"]["company_number"]
+                            new_c = Company(API.get_company_info(new_num))
+                            company_graph.add(new_c, o)
+                            ## @TODO: Multiple calls to the ssame data
+                            new_number_list.add(new_num)
 
-                
-        depth = depth - 1
+        q.append(new_number_list)
 
     return company_graph
 
@@ -78,83 +109,9 @@ class Officer:
 class Company:
     def __init__(self, json):
         self.has_insolvency_history = json["has_insolvency_history"]
-        self.active = True if json["status"] == "active" else False
+        ## Inconsistant json..
+        # self.active = True if json["status"] == "active" else False
         self.company_number = json["company_number"]
         self.company_name = json["company_name"]
         self.creation = json["date_of_creation"]
-
-comp = start_at_company("07798925")
-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import dash_cytoscape as cyto
-from dash.dependencies import Input, Output
-import plotly.express as px
-
-def return_company_graph(graph):
-    elements = []
-    for key, value in graph.graph_dict.items():
-        if type(key) is Company:
-            elements.append({"data":
-                             {
-                                 "id": key.company_name,
-                                 "label": key.company_name
-                             },
-                             "classes": "company"})
-            for elem in value:
-                elements.append({"data":
-                                 {
-                                     "source": key.company_name,
-                                     "target": elem.officer_name
-                                 }})
-        else:
-            elements.append({"data":
-                             {
-                                 "id": key.officer_name,
-                                 "label": key.officer_name
-                             },
-                             "classes": "officer"})
-            for elem in value:
-                elements.append({"data":
-                                 {
-                                     "source": key.officer_name,
-                                     "target": elem.company_name
-                                 }})
-
-    return elements
-
-app = dash.Dash(__name__)
-
-app.layout = html.Div([
-    html.P("Dash Cytoscape:"),
-    cyto.Cytoscape(
-        id='cytoscape',
-        elements=return_company_graph(comp),
-        layout={'name': 'breadthfirst'},
-        style={'width': '100%', 'height': '480px'},
-        stylesheet=[
-            {
-                'selector': 'node',
-                'style': {
-                    'content': 'data(label)'
-                }
-            },
-            {
-                'selector': '.officer',
-                'style': {
-                    'background-color': 'red',
-                }
-            },
-            {
-                'selector': '.company',
-                'style': {
-                    'background-color': 'blue',
-                }
-            }
-        ]
-    )
-])
-
-app.run_server(debug=True)
 
